e501008808db6d2cb05bfc60bc7db70d7f2e6a88
unknown <kbc16a24@stu.kawahara.ac.jp>
2019-11-22 10:07:11 +0900

    Add function of SampleAction2

--- src/main/java/jp/gihyo/jenkinsbook/action/SampleAction.java
+++ src/main/java/jp/gihyo/jenkinsbook/action/SampleAction.java
@@ -68,7 +68,7 @@ public class SampleAction {
         // Select the greeting message according to the time
         Calendar calendar = Calendar.getInstance();
         int hour = calendar.get(Calendar.HOUR_OF_DAY);
-        if (four < 12) {
+        if (hour < 12) {
           dto.setMessage("Good morning");
         } else {
           dto.setMessage("Good afternoon");
